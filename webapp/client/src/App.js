ac74889f5bb473ce96f88969efe0baaf1f04c4e7
Hizrok <jan.kapsa@seznam.cz>
2020-11-19 23:27:16 +0100

    projet initialization
    - created new react app
    - installed react bootstrap
    - checked if everything was working
    - added a gitignore file for node modules

---
+++ webapp/client/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div>
+      <Button>Click me!</Button>
+    </div>
+  );
+}
+
+export default App;
